4e6a462ae6e84bc2a1f90a91d0372d8c095ec73a
Andrew Drummond <andrew.drummond@plixer.com>
2019-08-27 17:31:50 -0600

    initial travis build

--- calc.pl
+++ calc.pl
@@ -1,6 +1,10 @@
 use warnings;    # this warns you of bad practices
 use strict;      # this prevents silly errors
 
+package Exile::Calc;
+
+use vars '$VERSION'; $VERSION = '1.0.0';
+
 sub add
 {
 	my ($a, $b) = @_;
